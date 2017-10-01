d615c5abae59f2c98ad2d3d347659c50dc576fb6
Aaron <aaron@asouer.com>
2017-09-30 22:59:19 -0500

    sql alchemy working

--- pyBook/models.py
+++ pyBook/models.py
@@ -28,36 +28,36 @@ class User(Base):
     def __repr__(self):
         return '<User %r>' % self._user_name
 
-    @property
-    def admin(self):
-        if self._admin == 1:
-            return True
-        else:
-            return False
-
-    @property
-    def user_name(self):
-        return self._user_name
-
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def first_name(self):
-        return self._first_name
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @property
-    def salt(self):
-        return self._salt
-
-    @property
-    def hash(self):
-        return self._hash
+    # @property
+    # def admin(self):
+    #     if self._admin == 1:
+    #         return True
+    #     else:
+    #         return False
+    #
+    # @property
+    # def user_name(self):
+    #     return self._user_name
+    #
+    # @property
+    # def email(self):
+    #     return self._email
+    #
+    # @property
+    # def first_name(self):
+    #     return self._first_name
+    #
+    # @property
+    # def last_name(self):
+    #     return self._last_name
+    #
+    # @property
+    # def salt(self):
+    #     return self._salt
+    #
+    # @property
+    # def hash(self):
+    #     return self._hash
 
 
 class Book(Base):
@@ -70,60 +70,77 @@ class Book(Base):
     author_last_name = Column(String(50))
     status = Column(SmallInteger)
     synopsis = Column(String(1000))
-    image = Column(String(100))
+    book_image = Column(String(100))
 
     def __init__(self, title=None, isbn_ten=None, isbn_thirteen=None,
                  author_first_name=None, author_last_name=None, status=0, synopsis=None,
-                 image='default.jpg', image_path = "static/img/covers/", book_id=None):
-        self._book_id = book_id
-        self._title = title
-        self._isbn_10 = isbn_ten
-        self._isbn_13 = isbn_thirteen
-        self._author_first_name = author_first_name
-        self._author_last_name = author_last_name
-        self._status = status
-        self._synopsis = synopsis
-        self._image = image
-        self._image_path = image_path
+                 image='default.jpg', book_id=None):
+        self.book_id = book_id
+        self.book_title = title
+        self.isbn_ten = isbn_ten
+        self.isbn_thirteen = isbn_thirteen
+        self.author_first_name = author_first_name
+        self.author_last_name = author_last_name
+        self.status = status
+        self.synopsis = synopsis
+        self.book_image = image
 
     def __repr__(self):
-        return '<Book %r>' % self._title
+        return '<Book %r>' % self.title
 
-    @property
-    def id(self):
-        return self._book_id
-
-    @property
-    def title(self):
-        return self._title
-
-    @property
-    def isbn_10(self):
-        return str(self._isbn_10).zfill(10)
-
-    @property
-    def isbn_13(self):
-        return str(self._isbn_13).zfill(13)
-
-    @property
-    def author_first_name(self):
-        return self._author_first_name
-
-    @property
-    def author_last_name(self):
-        return self._author_last_name
-
-    @property
-    def status(self):
-        if self._status == 1:
-            return True
-        else:
-            return False
-
-    @property
-    def synopsis(self):
-        return self._synopsis
+    def json(self):
+        return dict(title=self.title, isbn_10=self.isbn_10, isbn_13=self.isbn_13, author=self.author_first_name +
+             " " + self.author_last_name, status=self.status, synopsis=self.synopsis, image=self.image)
 
     @property
     def image(self):
-        return self._image_path + self._image
+        return "static/img/covers/" + self.book_image
+
+    @property
+    def title(self):
+        return str(self.book_title)
+
+    @property
+    def isbn_10(self):
+        return str(self.isbn_ten).zfill(10)
+
+    @property
+    def isbn_13(self):
+        return str(self.isbn_thirteen).zfill(13)
+
+
+    # @property
+    # def id(self):
+    #     return self._book_id
+    #
+    # @property
+    # def title(self):
+    #     return self._title
+    #
+    # @property
+    # def isbn_10(self):
+    #     return str(self._isbn_10).zfill(10)
+    #
+    # @property
+    # def isbn_13(self):
+    #     return str(self._isbn_13).zfill(13)
+    #
+    # @property
+    # def author_first_name(self):
+    #     return self._author_first_name
+    #
+    # @property
+    # def author_last_name(self):
+    #     return self._author_last_name
+    #
+    # @property
+    # def status(self):
+    #     if self._status == 1:
+    #         return True
+    #     else:
+    #         return False
+    #
+    # @property
+    # def synopsis(self):
+    #     return self._synopsis
+    #
--- pyBook/utils/files.py
+++ pyBook/utils/files.py
@@ -1,6 +1,4 @@
-import pyBook, os, urllib
-from flask import request, redirect, url_for
-from werkzeug.utils import secure_filename
+import os
 
 
 def exists(file):
@@ -8,56 +6,3 @@ def exists(file):
         return True
     else:
         return False
-
-# changes placeholder in config file to given value
-def updateConfig(placeholder, value):
-    conf = pyBook.app.root_path[:-7] + "/config.py"
-    with open(conf) as f:
-        s = f.read()
-        print(s)
-        if placeholder not in s:
-            print( '"{placeholder}" not found in {conf}.'.format(**locals()) )
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(conf, 'w') as f:
-        print( 'Changing "{placeholder}" to "{value}" in {conf}'.format(**locals()) )
-        s = s.replace(placeholder, value)
-        f.write(s)
-
-
-# tests if file type is an allowed extention
-def allowed(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in pyBook.app.config['ALLOWED_EXTENSIONS']
-
-
-# TODO add flash messages to function
-def upload(file):
-    if request.method == 'Post':
-        if 'file' not in request.files:
-            return redirect(url_for('library.index'))
-        if file and allowed(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(pyBook.app.config['COVER_UPLOAD_FOLDER'], filename)
-            return redirect(url_for('library.index'))
-        return redirect(url_for('library.index'))
-    return redirect(url_for('library.index'))
-
-
-def retrieve(url, file):
-    file = file.lower()
-    file = file.replace(" ", "")
-    save_to = pyBook.app.config['COVER_UPLOAD_FOLDER'] + "/" + file
-    if not exists(save_to):
-        print("creating " + file)
-        with open(save_to, "wb") as f:
-            f.write(urllib.request.urlopen(url).read())
-            f.close()
-    else:
-        print(file + " exists")
-
-img_url = 'http://covers.openlibrary.org/b/isbn/0385472579-L.jpg'
-
-retrieve(img_url, "Test 123456678.jpg")
-
